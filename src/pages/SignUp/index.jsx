6f521edca8858e95df3ecd4c74a78a58939a4a0f
Jordan Guimarães <jordanggomes@gmail.com>
2023-11-20 19:58:42 -0300

    Integração front-end e back-end

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,5 @@
 import { Container, Form, Background } from './style'
+import { useState } from 'react'
 import { Input } from '../../Components/Input'
 import { Button } from '../../Components/Button'
 import { BsPerson } from 'react-icons/bs'
@@ -7,6 +8,14 @@ import { ButtonText } from '../../Components/ButtonText'
 import { Link } from 'react-router-dom'
 
 export function SignUP() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert('prencha todos campos!')
+    }
+  }
   return (
     <Container>
       <Background />
@@ -16,10 +25,25 @@ export function SignUP() {
           <p>Aplicação para acompanhar tudo que assistir.</p>
         </div>
         <h2>Crie sua conta</h2>
-        <Input placeholder="Nome" type="text" icon={BsPerson} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
-        <Button title="Cadastrar" />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={BsPerson}
+          onChange={e => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
         <Link className="link" to="/">
           <ButtonText icon={FiArrowLeft} title="Voltar para o login" />
